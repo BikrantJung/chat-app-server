da23f1967c9ffe954a47cd00b0a4086476ab7dd3
Bikrant Jung <bikrantjung1214@gmail.com>
2023-04-20 13:08:31 +0545

    Search User

---
+++ src/controllers/user/getUser.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { User } from "../../models/user.model";
+import { CustomRequest } from "../../types/request.type";
+/**
+ * Search for users
+ * @route /api/user/getUser?search=
+ * @returns All users is query string not provided
+ * @returns Only one user if query is provided ?search=John
+ *
+ */
+
+export const getUser = async (req: CustomRequest, res: Response) => {
+  const query = req.query.search
+    ? {
+        $or: [
+          { username: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+
+  const users = await User.find(query);
+  res.status(200).json(users);
+};
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,7 +1,8 @@
-import { loginUser } from "../controllers/user";
+import { getUser, loginUser } from "../controllers/user";
 import { registerUser } from "../controllers/user";
 import express from "express";
 export const userRoutes = express.Router();
 
 userRoutes.post("/register", registerUser);
 userRoutes.post("/login", loginUser);
+userRoutes.get("/getUser", getUser);
